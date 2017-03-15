286a8fcbbc86df4f8275b588268fd1eb3c2c8706
Dag Frode Solberg <dag.frode.solberg@gmail.com>
2017-03-15 19:44:13 +0100

    Added the possibility to upload images

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import base64
+
 from flask import Flask, request, render_template, redirect, url_for
+from flask import make_response
 
 app = Flask(__name__)
 
@@ -23,20 +26,20 @@ def create_settings_dict(
 
 
 def get_values_from_settings(settings=create_settings_dict()):
-    return settings.get("project"), settings.get("description")
+    return settings.get("project"), settings.get("description"), settings.get("image")
 
 
 def get_settings():
     try:
         with open(SETTINGS_FILE) as data_file:
             data = json.load(data_file)
-            project, description = get_values_from_settings(data)
+            project, description, image = get_values_from_settings(data)
     except:
         save_settings_to_file(create_settings_dict())
         with open(SETTINGS_FILE) as data_file:
             data = json.load(data_file)
-            project, description = get_values_from_settings(data)
-    return project, description
+            project, description, image = get_values_from_settings(data)
+    return project, description, image
 
 
 def save_settings_to_file(values):
@@ -104,7 +107,7 @@ def watch_analog_input():
 @app.route('/')
 def hello_world():
     threading.Thread(target=indicateLoading).start()
-    project, description = get_settings()
+    project, description, image = get_settings()
     current_value = read_adc(potentiometer_adc)
     return render_template(
         'index.html',
@@ -112,16 +115,28 @@ def hello_world():
         description=description,
         current_value=current_value)
 
+@app.route("/image.jpg")
+def getImage():
+    _, _, image = get_settings()
+    response = make_response(base64.b64decode(image))
+    response.headers['Content-Type'] = 'image/jpeg'
+    response.headers['Content-Disposition'] = 'attachment; filename=img.jpg'
+    return response
+
 
 @app.route('/settings', methods=['GET', 'POST'])
 def settings():
     if request.method == 'POST':
         project = request.form.get("project")
         description = request.form.get("description")
-        save_settings_to_file({"project": project, "description": description})
+        image = request.files['image']
+        b64_img = base64.b64encode(image.read())
+        owner = request.form.get("owner")
+        number = request.form.get("number")
+        save_settings_to_file({"project": project, "description": description, "owner": owner, "number": number, "image": b64_img})
         return redirect(url_for('hello_world'))
 
-    project, description = get_settings()
+    project, description, image = get_settings()
 
     return render_template(
         "settings.html",
