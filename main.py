57b720b286392e153539113bd33e2753a0b50631
Dag Frode Solberg <dag.frode.solberg@gmail.com>
2017-03-16 11:37:58 +0100

    Refactored settings into own package

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import base64
+
 
 from flask import Flask, request, render_template, redirect, url_for
 from flask import make_response
@@ -6,46 +6,8 @@ from flask import make_response
 app = Flask(__name__)
 
 import time
-import os
 import threading
-import json
-from utils import read_adc
-
-# settings
-SETTINGS_FILE = os.path.dirname(os.path.realpath(__file__)) + '/settings.json'
-
-
-def create_settings_dict(
-        project="Nytt prosjekt",
-        description="Beskrivelse",
-):
-    return {
-        "project": project,
-        "description": description,
-    }
-
-
-def get_values_from_settings(settings=create_settings_dict()):
-    return settings.get("project"), settings.get("description"), settings.get("image")
-
-
-def get_settings():
-    try:
-        with open(SETTINGS_FILE) as data_file:
-            data = json.load(data_file)
-            project, description, image = get_values_from_settings(data)
-    except:
-        save_settings_to_file(create_settings_dict())
-        with open(SETTINGS_FILE) as data_file:
-            data = json.load(data_file)
-            project, description, image = get_values_from_settings(data)
-    return project, description, image
-
-
-def save_settings_to_file(values):
-    with open(SETTINGS_FILE, 'w') as outfile:
-        json.dump(values, outfile)
-
+from utils import read_adc, load_settings, save_settings, load_image, save_image
 
 DEBUG = 1
 
@@ -57,37 +19,37 @@ RED_LED_1 = 16
 RED_LED_2 = 20
 RED_LED_3 = 21
 
-#GPIO.setup(RED_LED_1, GPIO.OUT)
-#GPIO.setup(RED_LED_2, GPIO.OUT)
-#GPIO.setup(RED_LED_3, GPIO.OUT)
+# GPIO.setup(RED_LED_1, GPIO.OUT)
+# GPIO.setup(RED_LED_2, GPIO.OUT)
+# GPIO.setup(RED_LED_3, GPIO.OUT)
 
 # 10k trim pot connected to adc #0
 potentiometer_adc = 0
 
 
 def indicateLoading():
-   # GPIO.output(RED_LED_1, GPIO.HIGH)
+    # GPIO.output(RED_LED_1, GPIO.HIGH)
     time.sleep(1)
     print("led1")
-    #GPIO.output(RED_LED_1, GPIO.LOW)
-    #GPIO.output(RED_LED_2, GPIO.HIGH)
+    # GPIO.output(RED_LED_1, GPIO.LOW)
+    # GPIO.output(RED_LED_2, GPIO.HIGH)
     print("led2")
     time.sleep(1)
-    #GPIO.output(RED_LED_2, GPIO.LOW)
-    #GPIO.output(RED_LED_3, GPIO.HIGH)
+    # GPIO.output(RED_LED_2, GPIO.LOW)
+    # GPIO.output(RED_LED_3, GPIO.HIGH)
     print("led3")
     time.sleep(1)
-    #GPIO.output(RED_LED_3, GPIO.LOW)
+    # GPIO.output(RED_LED_3, GPIO.LOW)
 
 
 def show_activity():
-    #GPIO.output(RED_LED_1, GPIO.HIGH)
+    # GPIO.output(RED_LED_1, GPIO.HIGH)
     time.sleep(0.5)
-   # GPIO.output(RED_LED_1, GPIO.LOW)
+    # GPIO.output(RED_LED_1, GPIO.LOW)
     time.sleep(0.5)
-    #GPIO.output(RED_LED_1, GPIO.HIGH)
+    # GPIO.output(RED_LED_1, GPIO.HIGH)
     time.sleep(0.5)
-   # GPIO.output(RED_LED_1, GPIO.LOW)
+    # GPIO.output(RED_LED_1, GPIO.LOW)
 
 
 def watch_analog_input():
@@ -101,24 +63,24 @@ def watch_analog_input():
         time.sleep(1)
 
 
-#threading.Thread(target=watch_analog_input).start()
+# threading.Thread(target=watch_analog_input).start()
 
 
 @app.route('/')
 def hello_world():
     threading.Thread(target=indicateLoading).start()
-    project, description, image = get_settings()
+    settings = load_settings()
     current_value = read_adc(potentiometer_adc)
     return render_template(
         'index.html',
-        project=project,
-        description=description,
+        settings=settings,
         current_value=current_value)
+
 
 @app.route("/image.jpg")
 def getImage():
-    _, _, image = get_settings()
-    response = make_response(base64.b64decode(image))
+    image = load_image()
+    response = make_response(image)
     response.headers['Content-Type'] = 'image/jpeg'
     response.headers['Content-Disposition'] = 'attachment; filename=img.jpg'
     return response
@@ -127,48 +89,47 @@ def getImage():
 @app.route('/settings', methods=['GET', 'POST'])
 def settings():
     if request.method == 'POST':
-        project = request.form.get("project")
-        description = request.form.get("description")
-        image = request.files['image']
-        b64_img = base64.b64encode(image.read())
-        owner = request.form.get("owner")
-        number = request.form.get("number")
-        save_settings_to_file({"project": project, "description": description, "owner": owner, "number": number, "image": b64_img})
+        save_settings({
+            "project": request.form.get("project"),
+            "description": request.form.get("description"),
+            "owner": request.form.get("owner"),
+            "number": request.form.get("number")
+        })
+        save_image(request.files['image'])
         return redirect(url_for('hello_world'))
 
-    project, description, image = get_settings()
+    settings = load_settings()
 
     return render_template(
         "settings.html",
-        project=project,
-        description=description)
+        settings=settings)
 
 
 def startup():
     pass
-    #GPIO.output(RED_LED_1, GPIO.HIGH)
-    #GPIO.output(RED_LED_2, GPIO.HIGH)
-    #GPIO.output(RED_LED_3, GPIO.HIGH)
-    #time.sleep(0.5)
-    #GPIO.output(RED_LED_2, GPIO.LOW)
-    #GPIO.output(RED_LED_1, GPIO.LOW)
-    #GPIO.output(RED_LED_3, GPIO.LOW)
-    #time.sleep(0.5)
-    #GPIO.output(RED_LED_1, GPIO.HIGH)
-    #GPIO.output(RED_LED_2, GPIO.HIGH)
-    #GPIO.output(RED_LED_3, GPIO.HIGH)
-    #time.sleep(0.5)
-    #GPIO.output(RED_LED_2, GPIO.LOW)
-    #GPIO.output(RED_LED_1, GPIO.LOW)
-    #GPIO.output(RED_LED_3, GPIO.LOW)
-    #time.sleep(0.5)
-    #GPIO.output(RED_LED_1, GPIO.HIGH)
-    #GPIO.output(RED_LED_2, GPIO.HIGH)
-    #GPIO.output(RED_LED_3, GPIO.HIGH)
-    #time.sleep(0.5)
-    #GPIO.output(RED_LED_2, GPIO.LOW)
-    #GPIO.output(RED_LED_1, GPIO.LOW)
-    #GPIO.output(RED_LED_3, GPIO.LOW)
+    # GPIO.output(RED_LED_1, GPIO.HIGH)
+    # GPIO.output(RED_LED_2, GPIO.HIGH)
+    # GPIO.output(RED_LED_3, GPIO.HIGH)
+    # time.sleep(0.5)
+    # GPIO.output(RED_LED_2, GPIO.LOW)
+    # GPIO.output(RED_LED_1, GPIO.LOW)
+    # GPIO.output(RED_LED_3, GPIO.LOW)
+    # time.sleep(0.5)
+    # GPIO.output(RED_LED_1, GPIO.HIGH)
+    # GPIO.output(RED_LED_2, GPIO.HIGH)
+    # GPIO.output(RED_LED_3, GPIO.HIGH)
+    # time.sleep(0.5)
+    # GPIO.output(RED_LED_2, GPIO.LOW)
+    # GPIO.output(RED_LED_1, GPIO.LOW)
+    # GPIO.output(RED_LED_3, GPIO.LOW)
+    # time.sleep(0.5)
+    # GPIO.output(RED_LED_1, GPIO.HIGH)
+    # GPIO.output(RED_LED_2, GPIO.HIGH)
+    # GPIO.output(RED_LED_3, GPIO.HIGH)
+    # time.sleep(0.5)
+    # GPIO.output(RED_LED_2, GPIO.LOW)
+    # GPIO.output(RED_LED_1, GPIO.LOW)
+    # GPIO.output(RED_LED_3, GPIO.LOW)
 
 
 if __name__ == '__main__':
---
+++ utils/settings/settings.py
@@ -0,0 +1,73 @@
+import os
+import json
+from base64 import b64decode, b64encode
+
+# settings
+SETTINGS_FILE = os.path.dirname(os.path.realpath(__file__)) + '/settings.json'
+
+
+def create_settings_dict(
+        project="Nytt prosjekt",
+        description="Beskrivelse",
+        owner="navn",
+        number="78778"
+):
+    return {
+        "image": "",
+        "project": project,
+        "description": description,
+        "name": owner,
+        "number": number
+    }
+
+
+def get_values_from_settings(settings=create_settings_dict()):
+    return {
+        'number': settings.get("number"),
+        'owner': settings.get("owner"),
+        'project': settings.get("project"),
+        'description': settings.get("description"),
+    }
+
+
+def load_settings():
+    try:
+        with open(SETTINGS_FILE) as data_file:
+            data = json.load(data_file)
+    except:
+        save_settings(create_settings_dict())
+        with open(SETTINGS_FILE) as data_file:
+            data = json.load(data_file)
+    return get_values_from_settings(data)
+
+
+def save_settings(values):
+    with open(SETTINGS_FILE, 'w') as outfile:
+        json.dump(values, outfile)
+
+
+def load_image_from_file(settings=create_settings_dict()):
+    return b64decode(settings.get("image"))
+
+
+def load_image():
+    try:
+        with open(SETTINGS_FILE) as data_file:
+            data = json.load(data_file)
+    except:
+        save_settings(create_settings_dict())
+        with open(SETTINGS_FILE) as data_file:
+            data = json.load(data_file)
+    return load_image_from_file(data)
+
+
+def save_image(image):
+    try:
+        with open(SETTINGS_FILE) as data_file:
+            data = json.load(data_file)
+    except:
+        save_settings(create_settings_dict())
+        with open(SETTINGS_FILE) as data_file:
+            data = json.load(data_file)
+    data['image'] = b64encode(image.read())
+    save_settings(data)
